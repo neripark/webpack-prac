8ce003ec263daf104bf92996e41826b474f5d97c
neripark <neripark@gmail.com>
2018-09-15 17:22:45 +0900

    再生成功

--- src/components/yt-modal.vue
+++ src/components/yt-modal.vue
@@ -1,23 +1,70 @@
 <template>
   <div class="yt-modal">
     <div>
-      <p class="opener">これがオープンスイッチです。</p>
+      <p class="opener" @click="modalOpen">これがオープンスイッチです。</p>
     </div>
-    <div class="overlay"></div>
+    <transition class="fade">
+      <div class="yt-wrap" v-show="isModalOpen">
+        <div class="player-box">
+          <div id="player"></div>
+        </div>
+        <div class="overlay" @click="modalClose"></div>
+      </div>
+    </transition>
   </div>
 </template>
 
 <script>
+import Youtube from "../js/youtube.js";
+
 export default {
+  created() {
+    // this.yt = new Youtube("mNpPQXMgtmw");
+    // this.yt = new Youtube("236KUEBlXLE");
+  },
   data() {
     return {
-      hogehoge: true
+      isModalOpen: false
     };
+  },
+  methods: {
+    modalOpen() {
+      this.isModalOpen = true;
+      // this.yt = new Youtube("mNpPQXMgtmw");
+      this.yt = new Youtube("236KUEBlXLE"); // トーマス
+    },
+    modalClose() {
+      this.isModalOpen = false;
+    },
+
   }
 }
 </script>
 
 <style lang="scss" scoped>
+.yt-wrap {
+  .player-box {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    // width: 960px;
+    // height: 540px;
+    // background: #fff;
+    z-index: 2;
+  }
+
+  .overlay {
+    position: absolute;
+    width: 100vw;
+    height: 100vh;
+    top: 0;
+    left: 0;
+    background-color: #000;
+    opacity: 0.5;
+    z-index: 1;
+  }
+}
 .opener {
   background: #ccc;
   border-radius: 5px;
@@ -27,5 +74,12 @@ export default {
   &:hover {
     cursor: pointer;
   }
+}
+
+.v-enter-active, .v-leave-active {
+  transition: opacity .3s
+}
+.v-enter, .v-leave-to {
+  opacity: 0
 }
 </style>
---
+++ src/js/youtube.js
@@ -0,0 +1,62 @@
+// youtubeの動画を再生するスクリプト
+export default class youtube {
+
+  constructor(videoId) {
+    this.videoId = videoId;
+    this.init();
+  }
+
+  init() {
+    // youtube APIを読み込んで実行
+    const scriptTag = document.createElement("script");
+    scriptTag.src = this.youtubeApiUrl;
+    const fsTag = document.getElementsByTagName("script")[0];
+    fsTag.parentNode.insertBefore(scriptTag, fsTag);
+    window.onYouTubeIframeAPIReady = this.onReady(this.videoSetting);
+  }
+
+  // play() {
+  //   window.player.playVideo();
+  // }
+
+  // pause() {
+  //   window.player.pauseVideo();
+  // }
+
+  onReady(setting) {
+    return () => {
+      // グローバルな場所にプレイヤーをイニシャライズ
+      window.player = new YT.Player("player", setting);
+      console.log('The video is ready.');
+    }
+  }
+
+  get youtubeApiUrl() {
+    return "https://www.youtube.com/iframe_api";
+  }
+
+  set videoId(videoId) {
+    this._videoId = videoId;
+  }
+  get videoId() {
+    return this._videoId;
+  }
+
+  // 動画のセッティング
+  get videoSetting() {
+    return {
+      // height: "540",
+      // width: "960",
+      videoId: this.videoId,
+      playerVars: {
+        autohide: 1,
+        controls: 1,
+        modestbranding: 1,
+        iv_load_policy: 3,
+        showinfo: 0,
+        rel: 0,
+        autoplay: 0
+      }
+    };
+  }
+}
